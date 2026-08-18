1855ebe388dc5c83cdbd096e7d6b8281931603d4
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-08-19 01:35:48 +0500

    get routes

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ mongoose.connect(process.env.MONGO_URL);
 const contact_route = require("./routes/contactRoutes");
 
 app.use("/api", contact_route);
+app.get("/", (req,res)=>{
+    res.send("Hello world");
+})
 
 app.listen(8000, function(){
     console.log("server is running");
