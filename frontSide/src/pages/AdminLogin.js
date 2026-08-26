3c8caa1212ec3811abc04e59425302b9384621d1
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-08-26 14:33:11 +0500

    login page update

--- frontSide/src/pages/AdminLogin.js
+++ frontSide/src/pages/AdminLogin.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ContactServices from "../services/ContactServices";
+import "./AdminLogin.css";
 
 const AdminLogin = () => {
   const [email, setEmail] = useState("");
