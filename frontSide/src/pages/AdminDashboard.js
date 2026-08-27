711b63c1ba6e0d68fdece3bd946c8a8984bb7eac
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-08-27 15:56:43 +0500

    dashboard update css

--- frontSide/src/pages/AdminDashboard.js
+++ frontSide/src/pages/AdminDashboard.js
@@ -6,10 +6,11 @@ const AdminDashboard = () => {
   return (
     <>
       <AdminNavbar />
-
-      <div>
-        <h1>Admin Dashboard</h1>
-        <p>Welcome to your admin panel.</p>
+      <div className="admin-dashboard">
+        <div className="admin-dashboard-content">
+          <h1>Admin Dashboard</h1>
+          <p>Welcome to your admin panel.</p>
+        </div>
       </div>
     </>
   );
