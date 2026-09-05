caf4586f64a06b2fb6213fbfed03a8368e48321b
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-09-05 17:46:58 +0500

    add cloudinary

--- backend/controller/projectController.js
+++ backend/controller/projectController.js
@@ -1,18 +1,41 @@
 const Project = require('../models/projects');
+const cloudinary = require('../utils/cloudinary');
 
 // Create a new project
 const createProject = async (req, res) => {
     try {
         const { title, description, image, technologies, githubUrl, liveUrl, category, turnstileToken } = req.body;
+        // Check if the image is provided
+        if (!req.file) {
+            return res.status(400).send({ success: false, msg: "Image is required" });
+        }
+
+        // Upload the image to Cloudinary
+        const uploadResult = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream({
+                folder: "Projects",
+                resource_type: "auto"
+            }, (error, result) => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
+            stream.end(req.file.buffer);
+        });
+
+
         const project = new Project({
             title,
             description,
-            image,
+            image: uploadResult.secure_url,
             technologies,
             githubUrl,
             liveUrl,
-            category
+            category 
         });
+
         const data = await project.save();
         res.status(200).send({ success: true, msg: "Project created successfully", data: data });
 
