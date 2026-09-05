2870f541f8c7e238e1f8815e19998a0b705c1529
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-09-05 18:25:43 +0500

    add project update

--- frontSide/src/pages/projects/AddProject.js
+++ frontSide/src/pages/projects/AddProject.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import AdminNavbar from '../../components/AdminNavbar';
 import ProjectServices from '../../services/ProjectServices';
+import './AddProject.css';
 
 const AddProject = () => {
     const [formData, setFormData] = useState({
