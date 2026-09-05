6a1b967bc15d75e9c2d252b3b80fe504c68be0f4
Hassan-ccatel <hassan.ccatel@gmail.com>
2026-09-05 20:35:02 +0500

    icon update

--- frontSide/src/pages/projects/ViewAllProjects.js
+++ frontSide/src/pages/projects/ViewAllProjects.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { FaExternalLinkAlt, FaPaperPlane } from 'react-icons/fa';
 import ProjectsServices from '../../services/ProjectServices';
 
 const ViewAllProjects = () => {
